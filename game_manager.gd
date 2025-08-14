7cc3755afcdbf24748c5ab793f1c0a1271a4b750
J-AI4 <aishaani.jana@gmail.com>
2025-08-14 13:50:18 -0400

    plant card

--- game_manager.gd
+++ game_manager.gd
@@ -1,7 +1,16 @@
 extends Node2D
 
+var plant_card_active = false
+
 func get_nearest_tile():
 	var tilemap = $TileMap
 	var panda_pos = $Player.position
 	var cell = tilemap.world_to_map(panda_pos)
 	return cell
+
+
+func _on_plant_pressed() -> void:
+	plant_card_active = true
+
+func _process(delta):
+	if plant_card_active and Input.is_action_just_pressed("ui_select")
