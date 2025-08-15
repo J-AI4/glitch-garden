7429e7b8743a3d8f83f296149e3620786e2d7c33
J-AI4 <aishaani.jana@gmail.com>
2025-08-15 14:01:09 -0400

    Finally debugged movable camera
    After much trial and error, I finally implemented the movable camera.

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,8 +1,7 @@
 extends Camera2D
 
-var camera_speed = 300.0
-var min_bottom_y = 0.0
-var max_y = 999999.0
+var camera_speed = 600.0
+var min_bottom_y = -1000.0
 
 func _process(delta):
 	var movement = 0.0
@@ -14,12 +13,6 @@ func _process(delta):
 		
 	var half_height = (get_viewport_rect().size.y * zoom.y) / 2.0
 	
-	var min_camera_y = min_bottom_y - half_height
+	var lowest_y = min_bottom_y + half_height
 		
-	position.y = clamp(position.y + movement, min_camera_y, max_y)
-	
-	
-	
-	
-														  
-							   
+	position.y = clamp(position.y + movement, -INF, lowest_y)
